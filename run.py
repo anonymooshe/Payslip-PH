1f5c3e5168b9682e7e9f042c2308c9db02f1c65a
Jascha <syjascha@gmail.com>
2026-05-03 00:36:31 +0800

    Prod-ready: Flask refactor, print payslip, fix JS error, add favicon

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+import os
 from app import app
 from app.server import *
 
 if __name__ == "__main__":
-    print("  PaySlip PH backend running at http://localhost:8080")
-    app.run(debug=False, port=8080, host="127.0.0.1")
+    port = int(os.environ.get("PORT", 8080))
+    host = os.environ.get("HOST", "127.0.0.1")
+    app.run(debug=False, port=port, host=host)
